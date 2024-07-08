1c4d6fb4c76350b3dcc3228d38bbb4bd7fa0dbf5
ujwal1804 <ujwal.r@xrealty.ae>
2024-07-08 16:57:45 +0400

    base url changed

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3333/";
+export const BASE_URL = "http://localhost:3333/admin/";
 export const GET_ALL_USERS = BASE_URL + "getUsers";
 export const REGISTER_USER = BASE_URL + "register";
 export const UPDATE_USER = BASE_URL + "getUsers";
